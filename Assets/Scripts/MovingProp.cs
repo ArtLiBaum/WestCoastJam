9965a092f61d350ead5a6d623ac8edad24f005bb
Wander <a.baum@digipen.edu>
2022-07-29 22:26:17 -0700

    Added movement to level props

--- Assets/Scripts/MovingProp.cs
+++ Assets/Scripts/MovingProp.cs
@@ -3,18 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class MovingProp : MonoBehaviour
 {
-    [SerializeField] private float baseMoveSpeed = 1;
+    [Range(1,10)]
+    [SerializeField] private float baseMoveSpeed = 1f;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         //off screen check
+        if(transform.position.x < -10)
+            Destroy(gameObject);
         
+    }
+
+    private void FixedUpdate()
+    {
         //apply movement
-        Vector2 delta = new Vector2(baseMoveSpeed * GameManager.LevelSpeed + transform.position.x, 0);
-        transform.position = delta;
+        _rigidbody.MovePosition(transform.position + Vector3.right*(baseMoveSpeed * GameManager.LevelSpeed * Time.fixedDeltaTime) );
     }
 
     private void OnCollisionEnter2D(Collision2D col)
