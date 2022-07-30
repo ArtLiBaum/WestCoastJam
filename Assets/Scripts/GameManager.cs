c689dccc11fafc0cc38e2dd16a44af058f41fa07
GreenLog <84382400+GreenLog@users.noreply.github.com>
2022-07-30 15:32:59 -0700

    A variable that keeps track of how long the player has spent above the speed goal? In my game? More likely than you think.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,36 +5,46 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-   public static float LevelSpeed { get; set; } = -1f;
+    public static float LevelSpeed { get; set; } = -1f;
 
-   [SerializeField] private float LevelSpeedDecay = 0.1f;
-   private GameManager instance;
+    [SerializeField] private float LevelSpeedDecay = 0.1f;
+    [SerializeField] private float CoasterSpeedGoal = -5;
+    [SerializeField] private float CoasterTimeGoal = -5;
+
+    private float CoasterTimer = 0;
 
 
-   private void Update()
-   {
-      //TODO Slow the player over time
-      
-   }
+    private GameManager instance;
 
-   private void Awake()
-   {
-      if(instance)
-         Destroy(this);
-      else
-      {
-         instance = this;
-      }
-   }
-      
+
+    private void Update()
+    {
+        //TODO Slow the player over time
+
+    }
+
+    private void Awake()
+    {
+        if (instance)
+            Destroy(this);
+        else
+        {
+            instance = this;
+        }
+    }
+
     private void FixedUpdate()
     {
         print(LevelSpeed);
         LevelSpeed += LevelSpeedDecay * Time.deltaTime;
-       if (LevelSpeed > 0)
-       {
-            
-           LevelSpeed = 0;
-       }
+        if (LevelSpeed > 0)
+        {
+            LevelSpeed = 0;
+        }
+
+        if (LevelSpeed <= CoasterSpeedGoal)
+        {
+            CoasterTimer += Time.deltaTime;
+        }
     }
 }
