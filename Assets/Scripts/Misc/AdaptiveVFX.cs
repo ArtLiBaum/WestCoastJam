1f255f18002c66501255f0c6ac23de8c0475eac8
Charles O <mcfx2@mcfx2.co>
2022-07-31 00:45:31 -0700

    add vignette effect

--- Assets/Scripts/Misc/AdaptiveVFX.cs
+++ Assets/Scripts/Misc/AdaptiveVFX.cs
@@ -8,6 +8,7 @@ public class AdaptiveVFX : MonoBehaviour
 {
     private ColorGrading colorEffect;
     private Bloom bloom;
+    private Vignette vignette;
     
     [SerializeField] private Vector2 saturationScalingPoints = new Vector2(0, 2);
 
@@ -17,11 +18,16 @@ public class AdaptiveVFX : MonoBehaviour
 
     [SerializeField] private Vector2 bloomIntensityMinMax = new Vector2(0, 5);
 
+    [SerializeField] private Vector2 vignetteScalingPoints = new Vector2(0, 3);
+
+    [SerializeField] private Vector2 vignetteIntensityMinMax = new Vector2(0.5f, 0);
+
     private void Awake()
     {
         var volume = GetComponent<PostProcessVolume>();
         bloom = volume.profile.GetSetting<Bloom>();
         colorEffect = volume.profile.GetSetting<ColorGrading>();
+        vignette = volume.profile.GetSetting<Vignette>();
     }
 
     private static float GetTFromLevelSpeed(Vector2 scalePoints)
@@ -33,5 +39,6 @@ public class AdaptiveVFX : MonoBehaviour
     {
         bloom.intensity.Interp(bloomIntensityMinMax.x, bloomIntensityMinMax.y, GetTFromLevelSpeed(bloomIntensityScalingPoints));
         colorEffect.saturation.Interp(saturationMinMax.x, saturationMinMax.y, GetTFromLevelSpeed(saturationScalingPoints));
+        vignette.intensity.Interp(vignetteIntensityMinMax.x, vignetteIntensityMinMax.y, GetTFromLevelSpeed(vignetteScalingPoints));
     }
 }
