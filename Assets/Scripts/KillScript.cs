706fd214e514815c775633fadb6f4bf372b8fd0d
Wander <a.baum@digipen.edu>
2022-07-31 00:34:15 -0700

    Added Some animations and effects

---
+++ Assets/Scripts/KillScript.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillScript : MonoBehaviour
+{
+  public void Kill()
+  {
+    Destroy(gameObject);
+  }
+}
