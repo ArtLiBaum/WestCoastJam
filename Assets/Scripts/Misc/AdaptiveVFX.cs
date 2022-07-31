5fa235a9ce3f73c23651aea3af9bf2fb191456c6
Charles O <mcfx2@mcfx2.co>
2022-07-31 00:35:18 -0700

    vfx :)

---
+++ Assets/Scripts/Misc/AdaptiveVFX.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class AdaptiveVFX : MonoBehaviour
+{
+    private ColorGrading colorEffect;
+    private Bloom bloom;
+    
+    [SerializeField] private Vector2 saturationScalingPoints = new Vector2(0, 2);
+
+    [SerializeField] private Vector2 saturationMinMax = new Vector2(-100, 0);
+
+    [SerializeField] private Vector2 bloomIntensityScalingPoints = new Vector2(2, 4);
+
+    [SerializeField] private Vector2 bloomIntensityMinMax = new Vector2(0, 5);
+
+    private void Awake()
+    {
+        var volume = GetComponent<PostProcessVolume>();
+        bloom = volume.profile.GetSetting<Bloom>();
+        colorEffect = volume.profile.GetSetting<ColorGrading>();
+    }
+
+    private static float GetTFromLevelSpeed(Vector2 scalePoints)
+    {
+        return Mathf.Clamp01((Mathf.Abs(GameManager.LevelSpeed) - scalePoints.x) / (scalePoints.y - scalePoints.x));
+    }
+
+    private void Update()
+    {
+        bloom.intensity.Interp(bloomIntensityMinMax.x, bloomIntensityMinMax.y, GetTFromLevelSpeed(bloomIntensityScalingPoints));
+        colorEffect.saturation.Interp(saturationMinMax.x, saturationMinMax.y, GetTFromLevelSpeed(saturationScalingPoints));
+    }
+}
