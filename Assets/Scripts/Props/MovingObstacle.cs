2b331c4c1a4966bcd41997c387fa1e3e3b6e9c46
Charles O <mcfx2@mcfx2.co>
2022-07-30 23:04:02 -0700

    add screen shake

--- Assets/Scripts/Props/MovingObstacle.cs
+++ Assets/Scripts/Props/MovingObstacle.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class MovingObstacle : MovingProp
 {
     [SerializeField] private float penalty;
+    [SerializeField] private float screenShakeDur = 0.2f;
+    [SerializeField] private float screenShakeIntensity = 0.2f;
     
     protected override void OnHit()
     {
         GameManager.LevelSpeed += penalty;
+        CameraController.Main.Shake(screenShakeIntensity, screenShakeDur);
         Destroy(gameObject);
     }
 }
